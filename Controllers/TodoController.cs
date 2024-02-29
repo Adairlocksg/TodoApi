b0b445795e45a4cbe2114b7988a2f94bae9f233d
adair.grassi <adair.grassi@useall.com.bt>
2024-02-29 13:35:04 -0300

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using MeuTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("todos")]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+        }
+    }
+}
